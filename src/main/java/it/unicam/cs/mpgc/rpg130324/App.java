de87fde811fc33944e8886988cee2e733035a99f
MattiaSantinelli <mattia.santinelli@studenti.unicam.it>
2026-09-07 14:54:56 +0200

    Compattazione dei file App.java e Launcher.java all'interno di un unico file

--- src/main/java/it/unicam/cs/mpgc/rpg130324/App.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130324/App.java
@@ -1,7 +1,31 @@
 package it.unicam.cs.mpgc.rpg130324;
 
+import it.unicam.cs.mpgc.rpg130324.controller.GameController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class App {
+
     public static void main(String[] args) {
         Launcher.main(args);
+    }
+
+    // Deve essere public e static per permettere a JavaFX di istanziarla!
+    public static class Launcher extends Application {
+
+        public Launcher() {
+            // Costruttore no-arg esplicito per la reflection di JavaFX
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            // Inizializza il Controller e avvia il flusso del gioco
+            GameController gameController = new GameController(primaryStage);
+            gameController.avviaGioco();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
 }
